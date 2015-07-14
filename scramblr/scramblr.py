99bfe8849e1d519623993a0edaa1c9bbdc7ef39d
Haden Staab <cloudyhls@gmail.com>
2015-07-13 22:22:47 -0500

    added scrambler module

---
+++ scramblr/scramblr.py
@@ -0,0 +1,54 @@
+from crlib import encrypt, decrypt
+
+def welcome():
+    welcome_msg = """
+    =======================================
+    WELCOME TO SCRAMBLR!
+    Thanks for stopping by,
+
+    so..
+
+    What would you like to do?
+
+    0: Encrypt data
+    1: Decrypt data
+    2: Blow this popsickle stand!
+    =======================================
+    """
+    print welcome_msg
+
+
+def scrambl():
+    inp = raw_input('Please enter an encryption key:  ')
+    data = raw_input('Enter unencrypted data:  ')
+    scrambl = encrypt(inp, data)
+    f = open('cache/bc.store','w')
+    f.write(scrambl) # python will convert \n to os.linesep
+    f.close()
+    scramblr()
+def d_scrambl():
+    inp = raw_input('Please enter an encryption key:  ')
+    file = open('cache/bc.store','r')
+    data = file.read()
+    scrambl = decrypt(inp, data)
+
+    d_file = open('cache/ad.store','w')
+    d_file.write(scrambl)
+    scramblr()
+
+
+def scramblr():
+
+    direction = raw_input('Command: ')
+    if direction == '0':
+        scrambl()
+    elif direction == '1':
+        d_scrambl()
+    elif direction == '2':
+        print 'Peace \m/'
+
+def loop():
+    welcome()
+    scramblr()
+
+loop()
