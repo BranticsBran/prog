8871cdd4766a378e8d93ce67db9d49703d60d275
Brandon Balingit <brandonbalingit@Brandons-iMac.hsd1.in.comcast.net>
2015-07-13 19:55:16 -0500

    Setup basic application

---
+++ main.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+def main():
+    name = raw_input("What's your name boy?")
+
+    if name == 'haden':
+        print('FOGGOT')
+    else:
+        print('Ah youre ok')
+
+main()
