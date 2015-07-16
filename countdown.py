4d3dc89f3700744effbb87a52ac2614c643f6b44
bbalingit <balingit343@gmail.com>
2015-07-16 17:49:15 -0500

    started laying foundation for the countdown program.

--- countdown.py
+++
@@ -1,8 +0,0 @@
-"""This will be a program thats will let the user input a time
-limit, and begin a countdown."""
-
-def input_time():
-    time = raw_input("Please enter mm/dd/hh/ss:")
-    print time
-
-input_time()
